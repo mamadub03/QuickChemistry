8d6cb8ba2b2f73b05dccdbc9010172d07ff39ac4
mamadub03 <95324910+mamadub03@users.noreply.github.com>
2022-02-08 20:49:14 -0500

    Add pythonanywhere as an allowed host

--- QuickChemistry/settings.py
+++ QuickChemistry/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-8m3cj+dwj0s13dd-tm#q1xh&b1uyku7-4+*5y9*w$8(z%2k@39
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['quickchemistry.pythonanywhere.com']
 
 
 # Application definition
